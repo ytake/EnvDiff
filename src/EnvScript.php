10e5b20a7b5775a7e2e6505f2ca499c713a2494d
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2016-06-18 16:06:43 +0900

    init

--- src/EnvScript.php
+++ src/EnvScript.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Istyle\EnvDiff;
+namespace Ytake\EnvDiff;
 
+use ArrayIterator;
 use Composer\Script\Event;
 use josegonzalez\Dotenv\Loader;
 
@@ -10,19 +11,52 @@ use josegonzalez\Dotenv\Loader;
  */
 class EnvScript
 {
+    /** @var string */
+    protected static $filename = '.env';
+
     /**
      * @param Event $event
      */
     public static function postUpdate(Event $event)
     {
         $basisEnv = self::basisEnv();
-        var_dump($basisEnv);
+        $basis = new ArrayIterator($basisEnv);
+        $basis->ksort();
         $holder = [];
-        foreach (glob(getcwd() . '/{,.}env**', GLOB_BRACE + GLOB_NOSORT) as $filename) {
-            $Loader = new Loader($filename);
-            $holder[] = $Loader->parse()->toArray();
+        $iterator = [];
+        foreach (glob(getcwd() . "/" . self::$filename . '.*') as $filename) {
+
+            $comparison = new ArrayIterator(self::envParser($filename));
+            $comparison->ksort();
+            $iterator[basename($filename)] = $comparison;
+        }
+        foreach ($basis as $key => $item) {
+            foreach ($iterator as $file => $i) {
+                if (!isset($i[$key])) {
+                    $holder[$file][] = "ENVIRONMENT-NAME:[{$key}] not found.";
+                }
+            }
+        }
+        foreach ($iterator as $item) {
+            foreach($item as $key => $value) {
+                if(!isset($basis[$key])) {
+                    $holder[self::$filename][] = "ENVIRONMENT-NAME:[{$key}] not found.";
+                }
+            }
+        }
+        $io = $event->getIO();
+        if(count($holder)) {
+            foreach($holder as $key => $hold) {
+                $io->write(sprintf("<error>%s</error>", "[$key]  " . implode(" ", $hold)));
+            }
+            $solved = array_search('--force', $event->getArguments());
+            if($solved !== false) {
+                exit;
+            }
+            throw new \RuntimeException('.env error');
         }
     }
+
 
     /**
      * @return mixed
@@ -30,11 +64,22 @@ class EnvScript
      */
     protected static function basisEnv()
     {
-        $files = glob(getcwd() . '/{,.}env');
+        $files = glob(getcwd() . "/" . self::$filename);
         if (count($files)) {
-            return $files;
+            return self::envParser(self::$filename);
         }
 
-        throw new \Exception;
+        return false;
+    }
+
+    /**
+     * @param string $filename
+     * s@return array
+     */
+    private static function envParser($filename)
+    {
+        $Loader = new Loader($filename);
+
+        return $Loader->parse()->toArray();
     }
 }
